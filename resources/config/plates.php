51137896e8de47606e0d599497701ef9fdefe838
Vítor Brandão <vitor@noiselabs.org>
2014-03-25 23:56:01 +0000

    Add Module.php and resources/config/plates.php

---
+++ resources/config/plates.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @category    PPI
+ * @package     PlatesModule
+ * @copyright   Copyright (c) 2014 Paul Dragoonis <paul@ppi.io>
+ * @license     http://opensource.org/licenses/mit-license.php MIT
+ * @link        http://www.ppi.io
+ */
+
+/**
+ * Smarty options.
+ *
+ * The whole list can be seen here: {@link http://www.smarty.net/docs/en/api.variables.tpl}
+ */
+return array(
+    'options'   => array(
+        // ...
+    )
+);
