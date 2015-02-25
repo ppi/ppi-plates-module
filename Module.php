28dbebf7128bb7cd076981eba8ddcc8d405e1b02
Vítor Brandão <vitor@noiselabs.org>
2015-02-25 23:49:32 +0000

    Rename module to PlatesModule

--- Module.php
+++ Module.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the PPI Framework.
  *
- * @copyright   Copyright (c) 2014 Paul Dragoonis <paul@ppi.io>
+ * @copyright   Copyright (c) 2014-2015 Paul Dragoonis <paul@ppi.io>
  * @license     http://opensource.org/licenses/mit-license.php MIT
  * @link        http://www.ppi.io
  */
@@ -34,7 +34,7 @@ class Module extends AbstractModule
      */
     public function getName()
     {
-        return 'PPIPlatesModule';
+        return 'PlatesModule';
     }
 
     /**
