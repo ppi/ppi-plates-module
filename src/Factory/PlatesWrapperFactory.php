0b8d273200851af1cbfe4119e95b48df5650f65f
Paul Dragoonis <dragoonis@gmail.com>
2015-08-30 14:48:17 +0100

    Adding a factory for plates wrapper

---
+++ src/Factory/PlatesWrapperFactory.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of the PPI Framework.
+ *
+ * @copyright   Copyright (c) 2011-2013 Paul Dragoonis <paul@ppi.io>
+ * @license     http://opensource.org/licenses/mit-license.php MIT
+ *
+ * @link        http://www.ppi.io
+ */
+
+namespace PPI\PlatesTemplating\Factory;
+
+use League\Plates\Engine as PlatesEngine;
+use PPI\PlatesTemplating\PlatesWrapper;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * PlatesWrapperFactory
+ *
+ * @author Paul Dragoonis <paul@ppi.io>
+ */
+class PlatesWrapperFactory implements FactoryInterface
+{
+
+    protected $defaultConfig = [
+        'ext' => 'plate'
+    ];
+
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $platesEngine = new PlatesEngine();
+
+        $config = $serviceLocator->get('Config');
+        if (!isset($config['templating']['plates'])) {
+//            throw new \RuntimeException('No Plates Templating Configuration Found');
+            $platesConfig = $defaultConfig;
+        } else {
+            $defaultConfig = $config['templating']['plates'];
+        }
+
+        // @todo - config file ext
+        if (!isset($platesConfig['ext'])) {
+            $ext = self::$defaultConfig['ext'];
+        }
+
+        $platesEngine->setFileExtension($ext);
+
+        $platesWrapper = new PlatesWrapper($platesEngine);
+
+        return $platesWrapper;
+    }
